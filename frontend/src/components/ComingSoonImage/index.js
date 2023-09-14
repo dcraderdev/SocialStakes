ed77695d58df2d450a2e1bfe1b337b3af5fce658
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-09-13 21:15:36 -0700

    corrected stat page coming soon image

--- frontend/src/components/ComingSoonImage/index.js
+++ frontend/src/components/ComingSoonImage/index.js
@@ -1,18 +1,14 @@
 import { React } from 'react';
 import socialstakesCards2 from '../../images/socialstakes-logo-cards2.svg'
+import Logo from '../Logo';
 import './ComingSoonImage.css'
 
 const ComingSoonImage = () => {
 
   return (
-    <div className='comingsoon-container flex'>
-      <div className='comingsoon-image-wrapper flex center'>
-      <div className='comingsoon-image-container flex center'>
-        <img src={socialstakesCards2} alt="cards"></img>
-      </div>
-        <div className='comingsoon-name flex center'>SOCIAL STAKES</div> 
-      <div className='comingsoon-banner flex center'>Stats coming soon</div>
-      </div>
+    <div className='comingsoonimage-container comingsoonimage flex'>
+      <Logo />
+      <div className='comingsoonimage-name flex center'>Stats coming soon!</div>
     </div>
   )
 }
